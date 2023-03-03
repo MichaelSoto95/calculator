ac4ef6be7e7120feca8a7cf15caa979ab693229b
Michael Soto <msoto@ayacol.com>
2023-03-03 16:46:45 -0400

    work commit

--- calculator.js
+++ calculator.js
@@ -1,26 +1,45 @@
-const OpDiccionary ={'sum':'+',
-                   'min':'-',
-                   'mult':'*',
-                   'div':'/',
-                   'mod':'%'}
+   const OpDictionary ={sum :'+',
+                   min:'-',
+                   mult:'*',
+                   div:'/',
+                   mod:'%'} 
 
 
 //funcion que anade los elementos
 function placeItem(element){
+    let x ='sum'
     let item = element.id;
   let num= document.createElement('h1');
   num.setAttribute('id','calc-item');
   num.innerText = item;
    document.getElementById('result').append(num); 
-   console.log(OpDiccionary.item);
-   console.log(OpDiccionary.sum);
-
+  
+}
+//funcion que anade los operadores ej * + -
+function placeOperator(element){
+    let op = element.id;
+    let symbol= document.createElement('h1');
+    symbol.setAttribute('id','calc-item');
+    symbol.innerText = OpDictionary[op];
+    let  firstNum =Number(document.getElementById('result').textContent);
+    console.log(typeof(firstNum));
+  let  operator =symbol.textContent;
+    document.getElementById('result').append(symbol);
+      console.log(operator);
+    return operator;
+}
+function operator(){
 }
 
 
-//funcion que remueve un elemnto
+//funcion que remueve un elemento
 function removeItem(){
-    document.getElementById('calc-item').remove(); 
+    let res = document.getElementById('result').textContent;
+
+    if (res !=='' ) {
+        document.getElementById('calc-item').remove(); 
+    }
+    
 }
 //funcion que remueve todos los elmentos
 function clearAll(){
@@ -29,8 +48,23 @@ function clearAll(){
 
 
 function finalResult(){
-    let result= document.getElementById('result').textContent;
-    let finalresult = 0 + +parseInt(result);
-console.log(finalresult);
+    let result= document.getElementById('result').textContent;//tomamos el contenido de el div
+    let  res = result.split('*'); //dividimos el cintenisdo en un arrelo dividido por el operador
+    let oper =placeOperator();
+   console.log(oper);//Quero coger el valor del operador!!!!!!!!PENDIENTE
+//asigno valores
+let num1 = Number(res[0]);
+let num2 = Number(res[1]);
+console.log(num1);
+console.log(num2);
+//creamos variable de resultadofinal con la operacion matematica
+
+let endResult = num1 * num2;
+console.log(endResult);
+//cramos elemento H1
+let displayRes= document.createElement('h1');
+displayRes.innerText =endResult;
+clearAll();//borro el div
+document.getElementById('result').append(displayRes); //pongo el resultado
 
 }
